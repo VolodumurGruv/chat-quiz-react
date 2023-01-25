be2f95674ade52b414e5c48ded2a020a255ae151
VolodumurGrushka <gru.v@i.ua>
2023-01-25 23:59:17 +0200

    keep working

--- src/components/main/Questionare.js
+++ src/components/main/Questionare.js
@@ -1,17 +1,22 @@
 import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import { steper } from "./quizAPI";
-
+import { useDispatch, useSelector } from "react-redux";
+import { setStart, setIsQuiz } from "../../redux/slices/msgSlice";
+import { parseString } from "./parseString";
 import "../../styles/questions.scss";
 
 function Questionare() {
 	const [userAnswer, setUserAnswer] = useState("");
+	const [answers, setAnswers] = useState([]);
+	const [userScore, setUserScore] = useState(0);
+	const [showScore, setShowScore] = useState(false);
+	const dispatch = useDispatch();
+
+	const [step, setStep] = useState(0);
 	const { ques } = useSelector((state) => state.msg);
-	const step = steper(ques);
 
 	useEffect(() => {
-		console.log(userAnswer);
-	}, [userAnswer]);
+		setAnswers([...ques[step].incorrect_answers, ques[step].correct_answer]);
+	}, [step]);
 
 	const nextQuestion = (e) => {
 		if (e) {
@@ -19,27 +24,38 @@ function Questionare() {
 
 			setUserAnswer(e.target.innerText);
 		}
-
-		const question = step();
-		console.log(question);
-		const answers = [...question.incorrect_answers, question.correct_answer];
-
-		return (
-			<div className="questions">
-				<h3> {question.question} </h3>
-				<div className="btn__group">
-					{answers.map((ques, i) => (
-						<button key={i} className="btn btn__mt_30" onClick={nextQuestion}>
-							{ques}
-						</button>
-					))}
-				</div>
-			</div>
-		);
+		score();
+		setStep(step + 1);
+		if (step >= ques.length - 1) close();
 	};
+
+	const close = () => {
+		dispatch(setStart(false));
+		dispatch(setIsQuiz(false));
+		setStep(0);
+		setShowScore(true);
+	};
+
+	const score = () => {
+		if (userAnswer === ques[step].correct_answer) {
+			setUserScore(userScore + 1);
+		}
+
+		console.log(userScore);
+		console.log(userAnswer);
+		console.log(ques[step].correct_answer);
+	};
+
 	return (
-		<div>
-			<div>{nextQuestion()}</div>
+		<div className="questions">
+			<h3> {parseString(ques[step].question)} </h3>
+			<div className="btn__group">
+				{answers.map((ques, i) => (
+					<button key={i} className="btn btn__mt_30" onClick={nextQuestion}>
+						{ques}
+					</button>
+				))}
+			</div>
 		</div>
 	);
 }
--- src/components/main/quizAPI.js
+++ src/components/main/quizAPI.js
@@ -1,24 +1,8 @@
-export const steper = (arr) => {
-	let step = -1;
-
-	return () => {
-		if (!Array.isArray(arr))
-			throw new Error("Steper error: It should be an array!");
-
-		step += 1;
-
-		if (step >= arr.length) step = -1;
-
-		console.log(step);
-		return arr[step];
-	};
-};
-
 export const getQuizQuestions = async (fn) => {
 	const _URL = "https://opentdb.com/api.php?amount=10";
 
 	const data = await fetch(_URL);
 	const questions = await data.json();
-	console.log(questions?.results);
+
 	if (typeof fn === "function") fn(questions?.results);
 };
